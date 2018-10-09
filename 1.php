f4dc5126e04613a1b098f475693216d6f1c8c6ac
depong <37690969+depong@users.noreply.github.com>
2018-10-09 17:16:24 +0900

    20181009

---
+++ 1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="1-1.php">
+      <p>비밀번호를 입력해주세요.</p>
+      <input type="text" name="password">
+      <input type="submit">
+    </form>
+      <?php
+
+      ?>
+  </body>
+</html>
